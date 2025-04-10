eb8ac1dcd31c98b714ac3cf6a98dcac327f9269a
mollinaca <mail@mollinaca.dev>
2025-04-10 22:45:06 +0900

    update

--- discord-test/post.py
+++ discord-test/post.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import datetime
 import os
+import sys
 import requests
 #from dotenv import load_dotenv
 #load_dotenv()  # take environment variables
@@ -8,8 +10,13 @@ def main():
     # Webhook URL（先ほどコピーしたURLをここに貼り付け）
     WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL_TEST")
 
-    # 送信したいメッセージ
-    message = "こんにちは！これはWebhookを使って送信されたメッセージです。"
+    # message
+    if len(sys.argv) > 1:
+        # 引数がある場合、それを結合（複数行を含む1つの文字列として扱う）
+        message = " ".join(sys.argv[1:])
+    else:
+        # 引数がない場合はデフォルト値
+        message = DEFAULT_MESSAGE
 
     # メッセージのペイロードを設定
     payload = {
@@ -26,4 +33,6 @@ def main():
         print(f"メッセージ送信に失敗しました。ステータスコード: {response.status_code}")
 
 if __name__ == '__main__':
+    now_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    DEFAULT_MESSAGE = f"test from post.py : {now_str}"
     main()
